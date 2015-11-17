b80df65a4af36abe135d6aab089d230344ed827e
Kurt <kurt.du.bois@gmail.com>
2015-11-17 20:04:29 +0100

    Initial commit van de opzet voor spring boot.

---
+++ src/test/java/be/boks/RunApplicationTests.java
@@ -0,0 +1,16 @@
+package be.boks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RunApplication.class)
+public class RunApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
