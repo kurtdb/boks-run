e042832c346f1a181076c340c997f7ef0aa97c27
Kurt <kurt.du.bois@gmail.com>
2015-12-20 21:44:17 +0100

    Added save runner functionality.

--- src/main/java/be/boks/rest/RunnerController.java
+++ src/main/java/be/boks/rest/RunnerController.java
@@ -2,6 +2,8 @@ package be.boks.rest;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +15,17 @@ import be.boks.repository.RunnerRepository;
 @RequestMapping("/runner")
 public class RunnerController {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(RunnerController.class);
+	
 	@Autowired
 	private RunnerRepository runnerRepository;
 	
 	public List<Runner> getAll() {
 		return runnerRepository.findAll();
+	}
+	
+	public void register(Runner runner) {
+		Runner saved = runnerRepository.insert(runner);
+		LOGGER.info("Saved runner: " + saved);
 	}
 }
